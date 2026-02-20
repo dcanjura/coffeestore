4fb73eb0538a882035b48f5f6b23b8b25a6d736f
dcanjura <diegoac053@gmail.com>
2026-02-20 00:23:58 -0600

    Updated DTO to include mappers

--- src/main/java/com/exercise/coffeestore/dto/AdditionalDTO.java
+++ src/main/java/com/exercise/coffeestore/dto/AdditionalDTO.java
@@ -1,4 +1,23 @@
 package com.exercise.coffeestore.dto;
 
+import com.exercise.coffeestore.model.Additional;
+
 public record AdditionalDTO(Long id, String description, Double cost, Double total) {
+    public static Additional toAdditional(AdditionalDTO additionalDTO) {
+        return new Additional(
+                null,
+                additionalDTO.description(),
+                additionalDTO.cost(),
+                additionalDTO.total()
+        );
+    }
+
+    public static AdditionalDTO toAdditionalDTO(Additional additional) {
+        return new AdditionalDTO(
+                additional.getId(),
+                additional.getDescription(),
+                additional.getCost(),
+                additional.getCombineCost()
+        );
+    }
 }
