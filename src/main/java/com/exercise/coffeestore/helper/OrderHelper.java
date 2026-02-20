f4b585b7e6250c68e22ae752a0bfa659c8dba67b
dcanjura <diegoac053@gmail.com>
2026-02-20 00:23:46 -0600

    Updated helper, using DTO mappers

--- src/main/java/com/exercise/coffeestore/helper/OrderHelper.java
+++ src/main/java/com/exercise/coffeestore/helper/OrderHelper.java
@@ -3,10 +3,8 @@ package com.exercise.coffeestore.helper;
 import com.exercise.coffeestore.dto.AdditionalDTO;
 import com.exercise.coffeestore.dto.CoffeeDTO;
 import com.exercise.coffeestore.dto.OrderItemDTO;
-import com.exercise.coffeestore.mapper.AdditionalMapper;
-import com.exercise.coffeestore.mapper.CoffeeMapper;
 import com.exercise.coffeestore.model.Additional;
-import com.exercise.coffeestore.model.Coffee;
+import com.exercise.coffeestore.model.CoffeeEntity;
 
 import java.util.Comparator;
 import java.util.HashMap;
@@ -16,50 +14,47 @@ import java.util.stream.Collectors;
 
 public class OrderHelper {
 
-    private CoffeeMapper coffeeMapper;
-    private AdditionalMapper additionalMapper;
-
     /**
      * Calculates total amount for coffees and additional
-     * @param coffees
-     * @return
+     * @param coffees list
+     * @return a total amount
      */
-    public Double calculateTotal(List<Map<Coffee, List<Additional>>> coffees) {
+    public Double calculateTotal(List<Map<CoffeeEntity, List<Additional>>> coffees) {
         return coffees.stream()
                 .flatMap(map -> map.entrySet().stream())
                 .mapToDouble(entry -> {
-                    Coffee coffee = entry.getKey();
+                    CoffeeEntity coffeeEntity = entry.getKey();
                     List<Additional> additional = entry.getValue();
 
                     Double additionalTotal = additional.stream()
                             .mapToDouble(Additional::calculateCharge)
                             .sum();
 
-                    return coffee.getPrice() + additionalTotal;
+                    return coffeeEntity.getPrice() + additionalTotal;
                 })
                 .sum();
     }
 
-    public List<Map<Coffee, List<Additional>>> toDomain(List<OrderItemDTO> orderItemDTO) {
+    public List<Map<CoffeeEntity, List<Additional>>> toDomain(List<OrderItemDTO> orderItemDTO) {
         return orderItemDTO.stream()
                 .map(order -> {
-                    Coffee coffee = coffeeMapper.toCoffee(order.coffee());
+                    CoffeeEntity coffeeEntity = CoffeeDTO.toCoffee(order.coffee());
 
                     List<Additional> additional = order.additional()
                             .stream()
-                            .map(additionalMapper::toAdditional)
+                            .map(AdditionalDTO::toAdditional)
                             .toList();
 
-                    coffee.setName(buildItemName(coffee, additional));
-                    Map<Coffee, List<Additional>> map = new HashMap<>();
-                    map.put(coffee, additional);
+                    coffeeEntity.setName(buildItemName(coffeeEntity, additional));
+                    Map<CoffeeEntity, List<Additional>> map = new HashMap<>();
+                    map.put(coffeeEntity, additional);
 
                     return map;
                 })
                 .toList();
     }
 
-    private String buildItemName(Coffee coffee, List<Additional> additional) {
+    private String buildItemName(CoffeeEntity coffeeEntity, List<Additional> additional) {
 
         String additions = additional.stream()
                 .sorted(
@@ -67,23 +62,10 @@ public class OrderHelper {
                                 .comparing(Additional::getCost)
                                 .thenComparing(Additional::getDescription)
                 )
-                .map(a -> a.getDescription())
+                .map(Additional::getDescription)
                 .collect(Collectors.joining(" "));
 
-        return additions + " " + coffee.getName();
-    }
-
-    public List<OrderItemDTO> toOrderItemDTO(List<Map<Coffee, List<Additional>>> items) {
-
-        return items.stream()
-                .flatMap(map -> map.entrySet().stream())
-                .map(entry -> new OrderItemDTO(
-                        coffeeMapper.toCoffeeDTO(entry.getKey()),
-                        entry.getValue().stream()
-                                .map(additionalMapper::toAdditionalDTO)
-                                .toList()
-                ))
-                .toList();
+        return additions + " " + coffeeEntity.getName();
     }
 
 }
